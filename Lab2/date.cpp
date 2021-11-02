33b3b635ba2fa94fd85191a34b38f05f28e54ec1
Oleg <kombo007@mail.ru>
2021-11-02 22:13:12 +0700

    Сделан класс date

---
+++ Lab2/date.cpp
@@ -0,0 +1,57 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <conio.h>
+#include <stdio.h>
+#include "date.h"
+using namespace std;
+
+struct date enterDate() { //Функция ввода 
+	date data;
+	printf("\nВведите день: ");
+	while (scanf("%d", &data.day) != 1) {
+		while (getchar() != '\n');
+		printf("Ошибка. Введите число: ");
+	}
+	printf("Введите месяц: ");
+	while (scanf("%d", &data.month) != 1) {
+		while (getchar() != '\n');
+		printf("Ошибка. Введите число: ");
+	}
+	printf("Введите год: ");
+	while (scanf("%d", &data.year) != 1) {
+		while (getchar() != '\n');
+		printf("Ошибка. Введите число: ");
+	}
+	return data;
+}
+
+void printDate(date date1) { //Фукция вывода 
+	printf("%d.%d.%d", date1.day, date1.month, date1.year);
+}
+
+
+struct date createDate(int d, int m, int y) { //Функция создания 
+	date data;
+	data.day = d;
+	data.month = m;
+	data.year = y;
+	return data;
+}
+
+int difDate(date date1, date date2) {  //Функция, рассчитывающая количество дней
+	int kdate, sum1;
+	if (date1.month < 3) {
+		date1.year--, date1.month += 12;
+	}
+	kdate = 365 * date1.year + date1.year / 4 - date1.year / 100 + date1.year / 400 + (153 * date1.month - 457) / 5 + date1.day - 306;
+	if (date2.month < 3) {
+		date2.year--, date2.month += 12;
+	}
+	sum1 = 365 * date2.year + date2.year / 4 - date2.year / 100 + date2.year / 400 + (153 * date2.month - 457) / 5 + date2.day - 306;
+	if (kdate < sum1) {
+		printf("Ошибка. Вторая дата больше первой.");
+	}
+	else {
+		return kdate - sum1;
+	}
+}
